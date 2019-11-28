34682a98e6013625bf7abbcfac80e6040570da40
Karolina.Wielgus <karolina.wielgus@asseco.pl>
2019-11-28 21:59:23 +0100

    test5

--- src/com/wielgus/Main.java
+++ src/com/wielgus/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Test gitaa!");
         System.out.println("Test wspolny!");
         System.out.println("Test wspolny trzeci2!");
+        System.out.println("Test wspolny trzeci4!");
     }
 }
